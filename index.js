cc5c387f8b969eabd8edbb8994eca1ef5cf6065a
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-02-05 15:02:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+
+function findMatching(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+  }
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase().charAt(0) === string.toLowerCase().charAt(0)
+)}
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string)
+}
+// matchName()
+//   7) accesses the data structure to check if name matches
